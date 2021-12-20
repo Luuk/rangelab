c8f414ed34a36c8372f3e985821a83aa8ec3f22f
Luuk <34124413+Luuk@users.noreply.github.com>
2021-12-20 15:43:35 +0100

    Added side menu and login page

--- app/urls.py
+++ app/urls.py
@@ -4,4 +4,6 @@ from . import views
 
 urlpatterns = [
     path('overview', views.overview, name='overview'),
+    path('products', views.products, name='products'),
+    path('members', views.members, name='members'),
 ]
--- app/views.py
+++ app/views.py
@@ -5,3 +5,11 @@ from django.contrib.auth.decorators import login_required
 @login_required(login_url='login')
 def overview(request):
     return render(request, 'app/overview.html')
+
+@login_required(login_url='login')
+def products(request):
+    return render(request, 'app/overview.html')
+
+@login_required(login_url='login')
+def members(request):
+    return render(request, 'app/overview.html')
